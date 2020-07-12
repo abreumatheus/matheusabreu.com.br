fe3750c74c4051c6f5ce74e0787d11bdee9e6b4e
Matheus Abreu <abreumatheus@icloud.com>
2020-07-12 17:49:27 -0300

    Added Sitemap

--- nuxt.config.js
+++ nuxt.config.js
@@ -120,7 +120,11 @@ export default {
 					'Mozilla/5.0 (Linux; Android 5.1.1; Nexus 6 Build/LYZ28E) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.39 Mobile Safari/537.36',
 			},
 		],
+		'@nuxtjs/sitemap',
 	],
+	sitemap: {
+		hostname: 'https://matheusabreu.com.br',
+	},
 	toast: {
 		position: 'bottom-right',
 	},
